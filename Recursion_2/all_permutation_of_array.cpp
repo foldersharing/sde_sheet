a0492aaf927fc1b3b72088f22fb889703a508f52
foldersharing <109795448+foldersharing@users.noreply.github.com>
2023-08-27 21:29:18 +0530

    final code

--- Recursion_2/all_permutation_of_array.cpp
+++ Recursion_2/all_permutation_of_array.cpp
@@ -1,7 +1,46 @@
-# Program to find the all permutations of the array
+// To print all the permutations of the array using recursion and frequency table 
+
+
 
 #include<bits/stdc++.h>
-int main()
-{
-  
+
+using namespace std;
+class Solution {
+  private:
+    void recurPermute(vector < int > & ds, vector < int > & nums, vector < vector < int >> & ans, int freq[]) {
+      if (ds.size() == nums.size()) {
+        ans.push_back(ds);
+        return;
+      }
+      for (int i = 0; i < nums.size(); i++) {
+        if (!freq[i]) {
+          ds.push_back(nums[i]);
+          freq[i] = 1;
+          recurPermute(ds, nums, ans, freq);
+          freq[i] = 0;
+          ds.pop_back();
+        }
+      }
+    }
+  public:
+    vector < vector < int >> permute(vector < int > & nums) {
+      vector < vector < int >> ans;
+      vector < int > ds;
+      int freq[nums.size()];
+      for (int i = 0; i < nums.size(); i++) freq[i] = 0;
+      recurPermute(ds, nums, ans, freq);
+      return ans;
+    }
+};
+
+int main() {
+  Solution obj;
+  vector<int> v{1,2,3};
+  vector < vector < int >> sum = obj.permute(v);
+  cout << "All Permutations are " << endl;
+  for (int i = 0; i < sum.size(); i++) {
+    for (int j = 0; j < sum[i].size(); j++)
+      cout << sum[i][j] << " ";
+    cout << endl;
+  }
 }
